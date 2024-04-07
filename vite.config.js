3f04b035db4d7439b3e9ec3f991f2c9f306c9b85
evgeniy-shpyra <foksmix256@gmail.com>
2024-04-07 21:17:41 +0300

    init pwa app

---
+++ vite.config.js
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({ registerType: 'autoUpdate', outDir: 'dist', manifest: {
+      name: "Alarm",
+      short_name: "Home",
+      description: "Some description",
+      theme_color: "#fff",
+      icons: [
+        {
+          src: 'android-chrome-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'android-chrome-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ],
+      screenshots: {
+        
+      }
+    } }),
+  ],
+})
