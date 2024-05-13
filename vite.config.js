7cd411579109555c260937177d781e497328517e
Evgeniy <56407183+evgeniy-shpyra@users.noreply.github.com>
2024-05-13 21:19:45 +0300

    add screenshots

--- vite.config.js
+++ vite.config.js
@@ -5,26 +5,39 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({ registerType: 'autoUpdate', outDir: 'dist', manifest: {
-      name: "Alarm",
-      short_name: "Home",
-      description: "Some description",
-      theme_color: "#fff",
-      icons: [
-        {
-          src: 'android-chrome-192x192.png',
-          sizes: '192x192',
-          type: 'image/png'
-        },
-        {
-          src: 'android-chrome-512x512.png',
-          sizes: '512x512',
-          type: 'image/png'
-        }
-      ],
-      screenshots: {
-        
-      }
-    } }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      outDir: 'dist',
+      manifest: {
+        name: 'Alarm',
+        short_name: 'Home',
+        description: 'Some description',
+        theme_color: '#fff',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+        screenshots: [
+          {
+            src: '/android-chrome-192x192.png',
+            sizes: '1280x720',
+            type: 'image/png',
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '1280x720',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
   ],
 })
