d6591f67bbb0e993e4ad42fa58e14c2b0255546b
Evgeniy <56407183+evgeniy-shpyra@users.noreply.github.com>
2024-05-25 13:55:32 +0300

    fix host name

--- src/api/agent/index.js
+++ src/api/agent/index.js
@@ -1,2 +1,3 @@
 const hostname = "192.168.4.1"
+
 export const urlHttp = `http://${hostname}`
--- src/api/hub/index.js
+++ src/api/hub/index.js
@@ -1,5 +1,4 @@
-export const hubHostname = "192.168.31.132"
-// const hostname = window.location.hostname
+const hubHostname = window.location.hostname
 
 export const urlHttp = `http://${hubHostname}:9000`
 export const urlWs = `ws:${hubHostname}:9000`
